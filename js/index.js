ce0d4588413a17bdf7809da08e57693d9d07cdb5
Wei Da <weida.lee.2017@sis.smu.edu.sg>
2020-12-09 17:04:21 +0800

    update to my name

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 
 var descriptionCounter = 0;
-var description = 'Hello, my name is Marcus and I am an undergraduate based in Singapore, pursing a degree in Information System, who enjoys doing machine learning work';
+var description = 'Hello, my name is Marcus and I am an undergraduate based in Singapore, pursing a degree in Information System, majoring in Software Development';
 function descriptionTypeWriter() {
     if (descriptionCounter < description.length) {
         document.getElementById("description").innerHTML += description.charAt(descriptionCounter);
